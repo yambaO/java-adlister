cfcd58031613e2151f270ecc84de38b62dfb1a3d
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-05-09 09:54:00 -0500

    Adding servlets-start

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -2,6 +2,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
